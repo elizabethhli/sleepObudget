d703cf85ced10e8f0cc2cb42f2fd2e6e41ebc1f7
elizabethhli <elizabethyli@outlook.com>
2021-01-10 07:23:50 -0500

    some changes to budgetpage and cartpage

--- SheHacksApp - Copy/app/src/main/java/com/example/shehacksapp/BudgetPage.java
+++ SheHacksApp - Copy/app/src/main/java/com/example/shehacksapp/BudgetPage.java
@@ -51,6 +51,6 @@ public class BudgetPage extends AppCompatActivity {
 
             }
         });
-
     }
+
 }
--- SheHacksApp - Copy/app/src/main/java/com/example/shehacksapp/CartPage.java
+++ SheHacksApp - Copy/app/src/main/java/com/example/shehacksapp/CartPage.java
@@ -59,5 +59,43 @@ public class CartPage extends AppCompatActivity {
         customAdapter = new CustomAdapter(name, price,CartPage.this);
         items_list.setAdapter(customAdapter);
 
+        //code for the total price
+        double spent = CandyPage.getPrice() + ChipsPage.getPrice() + DinnerPage.getPrice() + DrinksPage.getPrice()
+                + EntertainmentPage.getPrice() + iceCreamPage.getPrice() + VenuePage.getPrice();
+​
+        int budget = (25*BudgetPage.getNumPeople() + 50);
+        if (spent > budget) {
+            System.out.println("Wow, you surpassed your budget by " + (int)(spent - budget)
+                    + "! Next time, try to think about the opportunity cost of your purchases."
+                    + "Opportunity cost is the cost of what you give up in choosing to buy your top choice!"
+                    + "For example, choosing to host your sleepover party at an Airbnb would cost you $210!"
+                    + "That only leaves you with $10 to buy food, drinks, and entertainment. Is it worth it?"
+                    + "Well, this varies from person to person. One person may find it totally worth it, "
+                    + "but another person may find it crazy how someone is willing to spend 95% of their budget on only the venue! "
+                    + "This is the idea of opportunity cost! Next time you make a purchase with your hard-earned money, "
+                    + "take some time to think about if the trade-off is worth it!");
+        }
+        else if (spent == budget) {
+            System.out.println("That was a close one! You spent your budget exactly! In the future, don't be afraid to "
+                    + "save a little extra next time. That way, you can choose to save the amount leftover, invest it to try and grow your money, "
+                    + "use it towards your next purchase, or however else you like! ");
+        }
+        else if (spent < (budget - 100)) {
+            System.out.println("Great job! You saved " + (int)(spent - budget)
+                    + "! Now you can use this extra money you saved to spend, invest, or save. It's up to you! "
+                    + "Keep up the great work! Now let's learn about why you were able to save so much: "
+                    + "Opportunity cost is the cost of what you give up in choosing to buy your top choice!"
+                    + "For example, choosing to host your sleepover party at an Airbnb would cost you $210!"
+                    + "That only leaves you with $10 to buy food, drinks, and entertainment. Is it worth it?"
+                    + "Based off of this game, you have shown that you are willing to make the decisions"
+                    + " that result in a lower opportunity cost. This means, you were willing to choose "
+                    + "products that made you give up less value. "
+                    + "The amount people value different products varies from person to person. Back to the example, "
+                    + "one person may find it totally worth it, but another person may find it crazy how someone is "
+                    + "willing to spend 95% of their budget on only the venue! "
+                    + "This is the idea of opportunity cost! Next time you make a purchase with your hard-earned money, "
+                    + "take some time to think about if the trade-off is worth it!");
+        }
+
     }
 }
